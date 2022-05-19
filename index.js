ec488e2fd77b53c1d84f17e6d34f571e0a9e0ddf
sumon092 <m.sumon@outlook.com>
2022-05-19 18:56:00 +0600

    error solved

--- index.js
+++ index.js
@@ -48,16 +48,33 @@ async function run() {
             res.send(users);
         })
 
-        app.put('/user/admin/:email', verifyJwt, async (req, res) => {
+        app.get('/admin/:email', async (req, res) => {
             const email = req.params.email;
-            const filter = { email: email };
-            const updateDoc = {
-                $set: { role: 'admin' },
-            }
-            const results = await usersCollection.updateOne(filter, updateDoc);
-            res.send(results)
+            console.log({ email });
+            const user = await usersCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin })
         })
-        app.put('/user/:email', verifyJwt, async (req, res) => {
+
+        app.put('/user/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            // const requester = req.decoded.email;
+            // const requesterAccount = await usersCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+                const filter = { email: email };
+                const updateDoc = {
+                    $set: { role: 'admin' },
+                };
+                const result = await usersCollection.updateOne(filter, updateDoc);
+                res.send(result);
+            }
+            else {
+                res.status(403).send({ message: 'forbidden' });
+            }
+
+        })
+
+        app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
             const user = req.body;
             const filter = { email: email };
@@ -113,18 +130,20 @@ async function run() {
         // })
 
 
-        app.get('/booking', verifyJwt, async (req, res) => {
+        app.get('/booking', async (req, res) => {
             const patient = req.query.patient;
+            console.log(patient);
             // const authorization = req.headers.authorization;
-            const decodedEmail = req.decoded.email;
-            if (decodedEmail) {
-                const query = { patient: patient }
-                const bookings = await bookingCollection.find(query).toArray();
-                return res.send(bookings);
-            }
-            else {
-                return res.status(403).send({ message: 'Forbidden Access' });
-            }
+            // const decodedEmail = req.decoded.email;
+            // if (decodedEmail) {
+            const query = { patient: patient }
+            console.log(query);
+            const bookings = await bookingCollection.find(query).toArray();
+            return res.send(bookings);
+            // }
+            // else {
+            //     return res.status(403).send({ message: 'Forbidden Access' });
+            // }
 
         });
 
